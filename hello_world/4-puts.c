52aa64ee06904ccdc284c55ab47861d6ca949a40
matteof7 <matteofoti741@gmail.com>
2024-10-21 12:13:49 +0200

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,3 +1,2 @@
-#!/bin/bash
-echo '#include <stdio.h>\nint main(void) { puts("\"Programming is like building a multilingual puzzle"); return 0; }' > 4-puts.c
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c && ./a.out
+#include <stdio.h>
+int main(void) { puts("\"Programming is like building a multilingual puzzle"); return 0; }
