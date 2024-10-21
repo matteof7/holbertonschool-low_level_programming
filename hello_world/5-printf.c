ccf46146887e8277eeb2566051ed8fb896d9afea
matteof7 <matteofoti741@gmail.com>
2024-10-21 12:33:26 +0200

    5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Point d'entrée du programme
+ *
+ * Return: Toujours 0 (Succès)
+ */
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
