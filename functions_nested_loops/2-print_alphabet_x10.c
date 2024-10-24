c097c15ff01a7cf9efea3fc0daf6a560dfca4e6e
matteof7 <matteofoti741@gmail.com>
2024-10-24 15:08:41 +0200

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,11 +2,12 @@
 
 void print_alphabet_x10(void)
 {
-const char *alphabet = abcdefghijklmnopqrstuvwxyz\n;
 int num;
-for (num = 0; num = < 10 num++)
+char letter;
+for (num = 0; num < 10; num++)
 {
-const char *current = alphabet;
-
-while (*current++);
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
 }
+} 
