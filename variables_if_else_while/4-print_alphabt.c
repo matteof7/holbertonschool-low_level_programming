59c4c0319fb2217179a211fb6e8a0c5c16b5abea
matteof7 <matteofoti741@gmail.com>
2024-10-24 11:03:41 +0200

    4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+/** 
+*main - Prints the alphabet in uppercase
+*
+*Description: This program prints the alphabet in uppercase,
+*followed by a new line. Only putchar can be used.
+*Return: Always 0 (Success)*/
+
 int main(void)
 {
-  char letter;
+char letter;
 
-  for (letter = 'a' ; letter <= 'z' ; letter++)
-    {
+for (letter = 'a' ; letter <= 'z' ; letter++)
+{
+
   if (letter == 'e' || letter == 'q')
-    continue;
-  putchar(letter);
-  }
-  putchar('\n');
+continue;
+putchar(letter);
+}
+putchar('\n');
 
-return(0);
+return (0);
 }
