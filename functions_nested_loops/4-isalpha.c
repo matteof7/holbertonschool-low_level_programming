84b87bf606e11437ff06923672267ca4c7590efa
matteof7 <matteofoti741@gmail.com>
2024-10-24 16:06:57 +0200

    4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 int _isalpha(int c)
 {
-if ((c > = 'a' && c <= 'z' || (c >= 'A' && c <= 'Z')
+if ((c > = 'a' && c <= 'z' || (c >= 'A' && c <= 'Z'))
 return (1);
 else
 return (0);
