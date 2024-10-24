00235670b3301aeb29c30fb67a0751672c440eba
matteof7 <matteofoti741@gmail.com>
2024-10-24 11:48:30 +0200

    5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+* main - Entry point of the program
+*
+* Description: Prints numbers from 0 to 9
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 int number;
