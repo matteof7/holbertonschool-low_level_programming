b7a4adab3d12eff1eeacf77aecd462c33754dca4
matteof7 <matteofoti741@gmail.com>
2024-10-28 11:26:40 +0100

    0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 
 int _isupper(int c)
-if (c >= 'A' && c <= 'Z') {
-    _putchar('1');
-    return (1);
-} else {
-    _putchar('0');
-    return (0);
+if (c >= 'A' && c <= 'Z')
+{
+_putchar('1');
+return (1);
+} else
+{
+_putchar('0');
+return (0);
 }
