bb4cdf072be0ee593e19f6b8231d404c2db9a661
matteof7 <matteofoti741@gmail.com>
2024-10-28 11:32:31 +0100

    0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+* _isupper - Vérifie si un caractère est une lettre majuscule
+* @c: Le caractère à vérifier
+*
+* Return: 1 si c est une majuscule, 0 sinon
+*/
 int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
