8ceb1727a104d53e3f7a61b2d333cbb113c73071
matteof7 <matteofoti741@gmail.com>
2024-10-29 09:54:25 +0100

    7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_line - Prints a straight line in the terminal
+* @n: The number of times the character '_' should be printed
+*
+* Description: If n is 0 or less, the function should only print '\n'
+*/
+void print_line(int n)
+{
+int i;
+
+if (n <= 0)
+{
+_putchar('\n');
+return;
+}
+
+for (i = 0; i < n; i++)
+{
+_putchar('\');
+}
+_putchar('\n');
+}
