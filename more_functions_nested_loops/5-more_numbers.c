944a98b7042c33d11f613a5839575ae0100d2a02
matteof7 <matteofoti741@gmail.com>
2024-10-29 09:09:42 +0100

    5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
+
 void more_numbers(void)
 {
-int i;
+int i, j;
 
-for (i = 0; i < 14; i++)
+for (i = 0; i < 10; i++)
 {
-_putchar(i + 0);
-}
+for (j = 0; j < 15; j++)
+{
+_putchar(j + '0');
 }
 _putchar('\n');
-return (10);
+}
 }
