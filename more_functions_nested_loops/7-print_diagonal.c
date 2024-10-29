3fe8a6ace1743245f1bd83919de698e0bd065e80
matteof7 <matteofoti741@gmail.com>
2024-10-29 10:02:29 +0100

    7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,13 +1,8 @@
 #include "main.h"
-/**
-* print_line - Prints a straight line in the terminal
-* @n: The number of times the character '\\' should be printed
-*
-* Description: If n is 0 or less, the function should only print '\n'
-*/
-void print_line(int n)
+
+void print_diagonal(int n)
 {
-int i;
+int i, j;
 
 if (n <= 0)
 {
@@ -17,7 +12,11 @@ return;
 
 for (i = 0; i < n; i++)
 {
-_putchar('\\');
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
 }
+_putchar('\\');
 _putchar('\n');
+}
 }
