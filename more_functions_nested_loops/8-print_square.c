be34c43630f7389f2f0d76ecfae00195466000db
matteof7 <matteofoti741@gmail.com>
2024-10-29 10:40:46 +0100

    8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -4,15 +4,15 @@ void print_square(int size)
 {
 int i, j;
 
-if (size = 0)
+if (size == 0)
 {
 _putchar('\n');
 return;
 }
 
-for (i = 0; < size; i++)
+for (i = 0; i < size; i++)
 {
-for (j = 0; < i; j++)
+for (j = 0; j < i; j++)
 {
 _putchar(' ');
 }
