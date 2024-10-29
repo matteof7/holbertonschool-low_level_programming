2e121b753d515312860e2caec7183c439bf3a214
matteof7 <matteofoti741@gmail.com>
2024-10-29 09:23:27 +0100

    5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+* more_numbers - Prints numbers from 0 to 14, ten times
+*
+* This function prints the numbers from 0 to 14, repeated 10 times.
+* Each number is printed digit by digit using _putchar.
+*/
 
 void more_numbers(void)
 {
