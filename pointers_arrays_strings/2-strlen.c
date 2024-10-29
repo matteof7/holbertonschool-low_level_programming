bf11d20c49a00e859b09741f90253ef6c5b545d9
matteof7 <matteofoti741@gmail.com>
2024-10-29 14:31:16 +0100

    2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+* _strlen - Calculates the length of a string
+* @s: The string to measure
+*
+* Return: The length of the string
+*/
 int _strlen(char *s)
 {
 int number = 0;
