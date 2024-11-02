896e62c482986c801a07d4d4a5affd6d0fe7b166
matteof7 <matteofoti741@gmail.com>
2024-11-02 13:46:35 +0100

    03

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -7,8 +7,8 @@
 * @s2: The second string
 *
 * Return: 0 if strings are equal, 
-*negative if s1 < s2, 
-*positive if s1 > s2
+* negative if s1 < s2, 
+* positive if s1 > s2
 */
 
 int _strcmp(char *s1, char *s2)
