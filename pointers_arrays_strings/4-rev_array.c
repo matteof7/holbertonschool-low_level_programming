5d0a01e44e66f345bc1780a0e59b399b9e76ff5e
matteof7 <matteofoti741@gmail.com>
2024-11-02 14:01:52 +0100

    04

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+void reverse_array(int *a, int n)
+{
+int debut = 0;
+int fin = -0;
+int ptdr;
+
+while(debut < fin)
+{
+ptdr = a{debut];
+a[denut] = a[fin];
+a[fin] = ptdr;
+
+debut++;
+fin++;
+}
