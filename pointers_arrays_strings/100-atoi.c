f280accb1837c04c606852be4e6e71fa50f3eca8
matteof7 <matteofoti741@gmail.com>
2024-11-02 12:43:07 +0100

    10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-
-int _atoi(char *s)
+#include <limits.h>
 
 /**
 * _atoi - Converts a string to an integer
@@ -14,11 +13,9 @@ int _atoi(char *s)
 int ptdr = 0;
 int lol = 1;
 int i = 0;
+int digit;
 
-while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
-i++;
-
-while (s[i] == '-' || s[i] == '+')
+while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
 {
 if (s[i] == '-')
 lol *= -1;
@@ -27,12 +24,15 @@ i++;
 
 while (s[i] >= '0' && s[i] <= '9')
 {
-if (ptdr > INT_MAX / 10 || (ptdr == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
+digit = s[i] - '0';
+if (ptdr > INT_MAX / 10 || (ptdr == INT_MAX / 10 && digit > INT_MAX % 10))
 return (lol == 1) ? INT_MAX : INT_MIN;
+if (ptdr < INT_MIN / 10 || (ptdr == INT_MIN / 10 && digit > -(INT_MIN % 10)))
+return INT_MIN;
 
-ptdr = ptdr * 10 + (s[i] - '0');
+ptdr = ptdr * 10 + lol * digit;
 i++;
 }
 
-return ptdr * lol;
+return ptdr;
 }
