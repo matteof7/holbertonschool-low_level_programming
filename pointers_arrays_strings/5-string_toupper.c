495ee5d867c55e55aef66611efc955296fabd1a3
matteof7 <matteofoti741@gmail.com>
2024-11-03 17:23:46 +0100

    05

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+char *string_toupper(char *)
+{
+int i = 0;
+    
+while (str[i] != '\0')
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 32;
+}
+i++;
+}
+
+return str;
+}
