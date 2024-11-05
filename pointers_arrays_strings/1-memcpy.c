36cdc5107e3eb0e066059c5d202e3b0575af657a
matteof7 <mattteofoti741@gmail.com>
2024-11-05 11:28:43 +0100

    001

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,16 @@
 #include "main.h"
 
+/**
+* _memcpy - Copies memory area
+* @dest: Pointer to the destination array where content will be copied
+* @src: Pointer to the source of data to be copied
+* @n: Number of bytes to copy
+*
+* Description: This function copies n bytes from memory area src
+* to memory area dest. The memory areas must not overlap.
+*
+* Return: A pointer to dest
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
