7a9500d82e646457e6f4954e433352f4ef34c696
matteof7 <mattteofoti741@gmail.com>
2024-11-05 10:20:23 +0100

    001

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -17,4 +17,5 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif
