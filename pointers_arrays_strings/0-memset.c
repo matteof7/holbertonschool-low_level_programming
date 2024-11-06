41372bcf5cf6829c42b31144024e10b610804f81
matteof7 <mattteofoti741@gmail.com>
2024-11-06 12:03:59 +0100

    000

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,16 +1,12 @@
-
+#include "main.h"
 /**
-* _memset - Fills memory with a constant byte
-* @s: Pointer to the memory area to be filled
-* @b: The constant byte to fill the memory with
-* @n: Number of bytes to be filled
-*
-* Description: This function fills the first n bytes of the memory area
-* pointed to by s with the constant byte b. It mimics the behavior of
-* the standard library function memset.
-*
-* Return: A pointer to the memory area s
-*/
+ * _memset - Fills memory with a constant byte
+ * @s: Pointer to the memory area to be filled
+ * @b: The constant byte to fill the memory with
+ * @n: Number of bytes to be filled
+ *
+ * Return: Pointer to the memory area s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
