6f0ffab31db652a71e8cd62458d9b28558d5676c
matteof7 <mattteofoti741@gmail.com>
2024-11-06 12:20:36 +0100

    002

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,14 +1,14 @@
 /**
- * _strchr - Locates a character in a string
- * @s: The string to be searched
- * @c: The character to find
- *
- * Description: This function searches for the first occurrence
- * of the character c in the string s.
- *
- * Return: A pointer to the first occurrence of the character c in the string s
- * or NULL if the character is not found
- */
+* _strchr - Locates a character in a string
+* @s: The string to be searched
+* @c: The character to find
+*
+* Description: This function searches for the first occurrence
+* of the character c in the string s.
+*
+* Return: A pointer to the first occurrence of the character c in the string s
+* or NULL if the character is not found
+*/
 char *_strchr(char *s, char c)
 {
 while (*s != '\0')
