fc5f9624fdebf6eee26939a78350754c1bf67717
matteof7 <mattteofoti741@gmail.com>
2024-11-06 10:32:51 +0100

    001

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,17 @@
 #include "main.h"
 
+/**
+* _strchr - Locates a character in a string
+* @s: The string to search
+* @c: The character to locate
+*
+* Description: This function searches for the first occurrence of the
+* character c in the string s. It mimics the behavior of the standard
+* library function strchr.
+*
+* Return: A pointer to the first occurrence of c in s, or NULL if c
+*         is not found
+*/
 char *_strchr(char *s, char c)
 {
 while (*s != '\0')
