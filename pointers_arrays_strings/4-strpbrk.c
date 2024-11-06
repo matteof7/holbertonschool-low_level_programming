102d365ff0a5c6225d66ee6bdf637bbe4c4eaa1a
matteof7 <mattteofoti741@gmail.com>
2024-11-06 12:29:59 +0100

    004

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -15,7 +15,6 @@
 char *_strpbrk(char *s, char *accept)
 {
 int i, j;
-
 for (i = 0; s[i] != '\0'; i++)
 {
 for (j = 0; accept[j] != '\0'; j++)
@@ -24,6 +23,5 @@ if (s[i] == accept[j])
 return (&s[i]);
 }
 }
-
 return (0);
 }
