e92676628fa5a9e9017d1c9ae3e5339c22e0b2ad
matteof7 <mattteofoti741@gmail.com>
2024-11-08 17:39:22 +0100

    0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,18 +2,13 @@
 
 /**
 * main - Prints the program name
-* @argc: The number of command line arguments
-* @argv: An array containing the program command line arguments
-*
-* Return: Always 0 (Success)
-*
-* Description: This program prints its own name, followed by a new line.
-* If the program is renamed, it will print the new name without
-* needing to be recompiled. The path before the name of the program
-* is not removed.
+* @argc: Number of command line arguments
+* @argv: Array of command line arguments
+* Return: 0 on success
 */
 int main(int argc, char *argv[])
 {
+(void)argc;
 printf("%s\n", argv[0]);
 return (0);
 }
